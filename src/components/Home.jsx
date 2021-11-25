bb9d18865270fcdcf9b7350894687ce88d51508e
sina tams <sina.tams@web.de>
2021-11-25 20:20:32 +0100

    join text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,23 @@
-import React, { Component } from 'react';
-import title from './img/howdiyGreen.png';
+import React, { Component } from "react";
+import title from "./img/howdiyGreen.png";
 
 export class Home extends Component {
-    render() {
-        return (
-            <div>
-            <img src={title} alt="title" className="title" />
-            <h3>DIY straight from your Pantry</h3>
-            <p>6 Categories to choose from:</p>
-            <h1>little representative icons?</h1>
-            <p>Join a community of DIY lovers who have plenty ideas on how to use the ingredients all around us to create fun useful homemade products! Find out more about how we started below:</p>
-            <button> to the Join!</button>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <img src={title} alt="title" className="title" />
+        <h3>DIY straight from your Pantry</h3>
+        <p>6 Categories to choose from:</p>
+        <h1>little representative icons?</h1>
+        <p>
+          Join our community of DIY lovers who have plenty ideas on how to use
+          the ingredients all around us to create fun useful homemade products!{" "}
+          <br /> Find out more about what drives us:
+        </p>
+        <button> to the Join!</button>
+      </div>
+    );
+  }
 }
 
-export default Home
+export default Home;
